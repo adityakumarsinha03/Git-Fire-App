9937b8f08b519a88de959e7196eb14985c13a7b0
Aditya <adityakumarsinha03@gmail.com>
2022-06-24 15:54:28 +0530

    first commit

---
+++ src/Firebase_config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDQlTqUbwYzdwjf-GLdINDPJtgHu2PcnXU",
+    authDomain: "mygitapp-92e58.firebaseapp.com",
+    projectId: "mygitapp-92e58",
+    storageBucket: "mygitapp-92e58.appspot.com",
+    messagingSenderId: "123902789219",
+    appId: "1:123902789219:web:043871c5d24d77e3ca1a56",
+    measurementId: "G-1NZG8XZCWZ"
+  };
